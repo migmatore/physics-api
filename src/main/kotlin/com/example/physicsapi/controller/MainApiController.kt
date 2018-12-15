d60a106b634f5a1f99288f8742d8849b4a06dc2e
moz200 <kazakvova201@gmail.com>
2018-12-15 15:11:08 +0300

    Add abstract class Math and add MainApiController(/api/minus)

--- src/main/kotlin/com/example/physicsapi/controller/MainApiController.kt
+++ src/main/kotlin/com/example/physicsapi/controller/MainApiController.kt
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import sun.security.util.math.IntegerModuloP
 
 @RestController
 @RequestMapping("/api")
 class MainApiController {
+    abstract class Math {
+        abstract var result: Int
+    }
 
     @GetMapping("/plus")
     fun plus(
@@ -16,11 +18,25 @@ class MainApiController {
             @RequestParam("num2") num2: Int
     ): Any {
         data class Plus(
-                val result: Int
-        )
+                override var result: Int
+        ) : Math()
 
         return Plus(
                 result = num1 + num2
+        )
+    }
+
+    @GetMapping("/minus")
+    fun minus(
+            @RequestParam("num1") num1: Int,
+            @RequestParam("num2") num2: Int
+    ): Any {
+        data class Minus(
+                override var result: Int
+        ) : Math()
+
+        return Minus(
+                result = num1 - num2
         )
     }
 }
